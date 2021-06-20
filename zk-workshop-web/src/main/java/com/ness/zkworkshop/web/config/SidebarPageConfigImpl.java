f98f2431d6fe3771d116b37e0f574e93008dfbcc
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2021-06-20 16:40:50 +0200

    zk-workshop-web, tabbox

--- zk-workshop-web/src/main/java/com/ness/zkworkshop/web/config/SidebarPageConfigImpl.java
+++ zk-workshop-web/src/main/java/com/ness/zkworkshop/web/config/SidebarPageConfigImpl.java
@@ -22,6 +22,7 @@ public class SidebarPageConfigImpl implements SidebarPageConfig{
 		pageMap.put("fn4",new SidebarPage("form", Labels.getLabel("web.userProfile") + " (MVVM)","/sources/imgs/profile.png","/pages/user-profile.zul", "z-icon-user"));
 		pageMap.put("fn5",new SidebarPage("todolist", Labels.getLabel("web.todoList") + " (MVVM)","/sources/imgs/todo.png","/pages/todolist.zul", "z-icon-list"));
 		pageMap.put("fn6",new SidebarPage("tree", "Tree" ,null,"/pages/tree.zul", "z-icon-sitemap"));
+		pageMap.put("fn7",new SidebarPage("tabbox", "Tabbox" ,null,"/pages/tabbox.zul", "z-icon-table"));
 	}
 	
 	public List<SidebarPage> getPages(){
