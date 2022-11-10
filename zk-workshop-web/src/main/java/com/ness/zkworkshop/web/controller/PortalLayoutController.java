6af879c4884bca22f1217495632a22508663f3d6
Jakub Zaoralek <jakub.zaoralek@gmail.com>
2022-11-10 22:17:24 +0100

    customizatio button generated in composer

--- zk-workshop-web/src/main/java/com/ness/zkworkshop/web/controller/PortalLayoutController.java
+++ zk-workshop-web/src/main/java/com/ness/zkworkshop/web/controller/PortalLayoutController.java
@@ -4,18 +4,18 @@ import com.ness.zkworkshop.web.config.DashboardPanelConfig;
 import com.ness.zkworkshop.web.model.DashboardPanel;
 import com.ness.zkworkshop.web.util.EventQueueHelper;
 import com.ness.zkworkshop.web.viewmodel.AddWidgetModalVM;
+import org.zkoss.util.resource.Labels;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zkex.zul.Colorbox;
 import org.zkoss.zkmax.zul.Portalchildren;
 import org.zkoss.zkmax.zul.Portallayout;
-import org.zkoss.zul.Calendar;
-import org.zkoss.zul.Include;
-import org.zkoss.zul.Panel;
-import org.zkoss.zul.Panelchildren;
+import org.zkoss.zul.*;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
@@ -107,10 +107,8 @@ public class PortalLayoutController extends SelectorComposer<Component> {
         panelToAdd.setStyle("margin-bottom:10px");
         panelToAdd.setSclass("portal-widget-panel");
 
-        updateEditModePanel(panelToAdd, editMode);
-
         if (!"".equals(panel.getPanelUri())) {
-            panelToAdd.addEventListener(Events.ON_CLICK, event -> Executions.sendRedirect(panel.getPanelUri()));
+            // panelToAdd.addEventListener(Events.ON_CLICK, event -> Executions.sendRedirect(panel.getPanelUri()));
         }
         Panelchildren panelchilds = new Panelchildren();
         panelchilds.setStyle("overflow-y: auto;");
@@ -130,8 +128,97 @@ public class PortalLayoutController extends SelectorComposer<Component> {
             panelToAdd.setStyle("background: #FFFFCC");
         }
 
-        firstChild.appendChild(panelToAdd);
+        // Add customization button.
+        addCustTools(panelchilds, firstChild, panelToAdd);
+
+        // Editacni rezim.
+        updateEditModePanel(panelToAdd, editMode);
+
         // saveStatus();
+    }
+
+    /**
+     * Pridani tlacitka s popupem pro customizaci panelu.
+     * @param panelchilds
+     * @param firstChild
+     * @param panelToAdd
+     */
+    private void addCustTools(Panelchildren panelchilds,
+                              Component firstChild,
+                              Panel panelToAdd) {
+        Hbox panelCustHbox = new Hbox();
+        panelCustHbox.setVisible(editMode);
+        panelCustHbox.setStyle("margin: 10px;");
+
+        // Button
+        Button panelCustBtn = new Button();
+        panelCustBtn.setIconSclass("z-icon-ellipsis-h");
+        panelCustBtn.setStyle("background: #F9FCFF; color: #8A8A8A;");
+        panelCustBtn.setTooltiptext(Labels.getLabel("web.settings"));
+        // Popup
+        Popup panelCustPopup = new Popup();
+        Vlayout panelCustPopupVlayout = new Vlayout();
+
+        Hlayout captionHlayout = new Hlayout();
+        captionHlayout.setSclass("z-caption-label");
+        captionHlayout.appendChild(new Label(Labels.getLabel("web.settings")));
+        panelCustPopupVlayout.appendChild(captionHlayout);
+
+        Grid panelCustSettingsGrid = new Grid();
+        panelCustSettingsGrid.setHflex("min");
+        Columns cols = new Columns();
+        cols.appendChild(new Column());
+        cols.appendChild(new Column());
+        panelCustSettingsGrid.appendChild(cols);
+        Rows rows = new Rows();
+
+        // Title row
+        Row titleRow = new Row();
+        titleRow.appendChild(new Label(Labels.getLabel("web.title")));
+        Textbox titleRowTxt = new Textbox(panelToAdd.getTitle());
+        titleRowTxt.addEventListener(Events.ON_CHANGE, event -> panelToAdd.setTitle(titleRowTxt.getValue()));
+        titleRowTxt.addEventListener(Events.ON_OK, event -> panelToAdd.setTitle(titleRowTxt.getValue()));
+        titleRow.appendChild(titleRowTxt);
+        rows.appendChild(titleRow);
+
+        // Background color
+        Row backgroundColorRow = new Row();
+        backgroundColorRow.appendChild(new Label(Labels.getLabel("web.header")));
+        Colorbox backgrColorbox = new Colorbox();
+        backgrColorbox.setColor("#F9FCFF");
+        backgroundColorRow.appendChild(backgrColorbox);
+        rows.appendChild(backgroundColorRow);
+
+        // Border color
+        Row borderColorRow = new Row();
+        borderColorRow.appendChild(new Label(Labels.getLabel("web.border")));
+        Colorbox borderColorBox = new Colorbox();
+        borderColorBox.setColor("#d9d9d9");
+        borderColorRow.appendChild(borderColorBox);
+
+        backgrColorbox.addEventListener(Events.ON_CHANGE, event -> setPanelStyle(panelToAdd, backgrColorbox.getColor(), borderColorBox.getColor()));
+        borderColorBox.addEventListener(Events.ON_CHANGE, event -> setPanelStyle(panelToAdd, backgrColorbox.getColor(), borderColorBox.getColor()));
+
+        rows.appendChild(borderColorRow);
+
+        panelCustSettingsGrid.appendChild(rows);
+        panelCustPopupVlayout.appendChild(panelCustSettingsGrid);
+        panelCustPopup.appendChild(panelCustPopupVlayout);
+
+        panelCustHbox.appendChild(panelCustPopup);
+        panelCustBtn.setPopup(panelCustPopup);
+        panelCustHbox.appendChild(panelCustBtn);
+
+        panelchilds.appendChild(panelCustHbox);
+
+        firstChild.appendChild(panelToAdd);
+    }
+
+    /**
+     * Nastaveni barvy pozadi a ohraniceni podle colorpickeru.
+     */
+    private void setPanelStyle(Panel panel, String backgrColor, String borderColor) {
+        panel.setStyle("background: " + backgrColor + "; border-color: " + borderColor);
     }
 
     private void updateEditMode(boolean editMode) {
@@ -152,5 +239,12 @@ public class PortalLayoutController extends SelectorComposer<Component> {
         panel.setSizable(editMode);
         panel.setMaximizable(!editMode);
         panel.setClosable(editMode);
+        // tlacitko pro customizaci
+        if (panel.getPanelchildren() != null) {
+            Component lastChild = panel.getPanelchildren().getLastChild();
+            if (lastChild instanceof Hbox) {
+                lastChild.setVisible(editMode);
+            }
+        }
     }
 }
